3b3f36bc6a48ad2aa4ad75ee3d0ced456bd4e060
DIVYA-19 <41481377+DIVYA-19@users.noreply.github.com>
2019-06-02 10:35:37 +0530

    Create longest_subsequence.py

---
+++ longest_subsequence.py
@@ -0,0 +1,25 @@
+#longest subsequence word by word
+s1 = "I am waiting"
+s2 ="I have been waiting"
+
+memo = [[-1] * len(s2) for _ in range(len(s1))]
+
+def LSS(s1,s2,i1,i2,memo): #Longest Sub Sequence
+    if i1 == len(s1) or i2 == len(s2):
+        return ''
+    if memo[i1][i2] != -1:
+        return memo[i1][i2]
+    if s1[i1] == s2[i2]:
+        memo[i1][i2] = s1[i1] + LSS(s1,s2,i1+1,i2+1,memo)
+        return memo[i1][i2]
+    resultA = LSS(s1,s2,i1+1,i2,memo)
+    resultB = LSS(s1,s2,i1,i2+1,memo)
+    if len(resultA) > len(resultB):
+        memo[i1][i2] = resultA
+        return resultA
+    else:
+        memo[i1][i2] = resultB
+        return resultB
+
+res = LSS(s1,s2,0,0,memo)
+print(res)
