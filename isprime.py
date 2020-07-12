23c4c847fb12441d33850580d0265bba0fcfbf40
Divya Yeruva <41481377+DIVYA-19@users.noreply.github.com>
2020-07-12 13:59:08 +0530

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,9 +1,12 @@
-n= int(input())
 
-for i in range(2, n):
-    if n%i == 0:
-        print("false")
-        break
-else:
-    print("true")
+
+def isPrime(num):
+    for i in range(2, n):
+        if n%i == 0:
+            return "false"
+    else:
+        return "true"
     
+if __name__ == "__main__":
+    n= int(input())
+    print(isPrime(n))
